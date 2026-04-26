4c7ef22e113560659cef2253ab69e4e043014b57
Guilherme Paes <paesguigo@gmail.com>
2026-04-26 20:31:29 -0300

    feat: add project into github

---
+++ editor-service/src/editor.gateway.ts
@@ -0,0 +1,30 @@
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway(3001, { cors: { origin: '*' } })
+export class EditorGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server!: Server;
+
+  handleConnection(client: Socket) {
+    console.log(`Cliente conectado: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Cliente desconectado: ${client.id}`);
+  }
+
+  @SubscribeMessage('edit-content')
+  // Envio para outros
+  handleEvent(@MessageBody() data: string): string {
+    this.server.emit('content-updated', data);
+    return data;
+  }
+}
