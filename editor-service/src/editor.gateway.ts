bdd5909d5978da4fcde517b6c0f659867d5c01b7
Guilherme Paes <paesguigo@gmail.com>
2026-05-30 20:43:41 -0300

    feat: validate jwt on editor-service gateway

--- editor-service/src/editor.gateway.ts
+++ editor-service/src/editor.gateway.ts
@@ -10,43 +10,87 @@ import {
   MessageBody,
   WebSocketServer,
   ConnectedSocket,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
+
 import { Server, Socket } from 'socket.io';
 import { PrismaService } from './prisma/prisma.service';
+import * as jwt from 'jsonwebtoken';
 
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
 })
-export class EditorGateway {
+export class EditorGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server!: Server;
 
   constructor(private readonly prisma: PrismaService) {}
 
+  handleConnection(client: Socket) {
+    try {
+      const token =
+        client.handshake.auth?.token ||
+        client.handshake.headers.authorization?.replace('Bearer ', '');
+
+      if (!token) {
+        console.log('Token não enviado');
+        client.disconnect();
+        return;
+      }
+
+      const payload = jwt.verify(token, process.env.JWT_SECRET as string);
+
+      client.data.user = payload;
+
+      console.log(`Usuário autenticado: ${(payload as any).email}`);
+    } catch (error) {
+      console.log('JWT inválido');
+      client.disconnect();
+    }
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Cliente desconectado ${client.id}`);
+  }
+
   @SubscribeMessage('join-document')
   async handleJoin(
     @ConnectedSocket() client: Socket,
-    @MessageBody() docId: string,
+    @MessageBody() data: { docId: string },
   ) {
-    client.join(docId);
-    console.log(`Cliente ${client.id} entrou no documento ${docId}`);
+    client.join(data.docId);
+
+    console.log(`${client.data.user.email} entrou no documento ${data.docId}`);
 
     const document = await this.prisma.document.findUnique({
-      where: { id: docId },
+      where: { id: data.docId },
     });
+
     if (document) {
       client.emit('load-document', document.content);
+
       console.log(`doc init enviado para o cliente ${client.id}`);
     }
   }
 
   @SubscribeMessage('edit-content')
-  async handleEdit(@MessageBody() data: { docId: string; content: string }) {
+  async handleEdit(
+    @MessageBody()
+    data: {
+      docId: string;
+      content: string;
+    },
+  ) {
+    console.log('Recebido:', data);
+
     const document = await this.prisma.document.upsert({
       where: { id: data.docId },
-      update: { content: data.content },
+      update: {
+        content: data.content,
+      },
       create: {
         id: data.docId,
         content: data.content,
